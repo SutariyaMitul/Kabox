6f81d3aa9567ed04be71fbdde01f8cf8cf93a42e
jgundhani <163627271+jgundhani@users.noreply.github.com>
2024-08-10 16:20:15 +0530

    new padding change

--- lib/employee_screen/choosepassword_screen/checkemail_screen.dart
+++ lib/employee_screen/choosepassword_screen/checkemail_screen.dart
@@ -3,10 +3,9 @@ import 'package:flutter_svg/flutter_svg.dart';
 import 'package:kbox/Common/common_scaffold.dart';
 import 'package:kbox/Common/common_text.dart';
 import 'package:kbox/Common/text_style.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 import '../../Common/common_color.dart';
-
-
 
 class CheckEmailScreen extends StatefulWidget {
   const CheckEmailScreen({super.key});
@@ -18,37 +17,42 @@ class CheckEmailScreen extends StatefulWidget {
 class _CheckEmailScreenState extends State<CheckEmailScreen> {
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: CommonScaffold(
-        bgColor: CommonColor.white,
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Padding(
-              padding: const EdgeInsets.only(top: 73),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.only(bottom: 28),
-                    child: SvgPicture.asset("assets/images/checkingmail.svg",height: 84,width: 84,),
+    return CommonScaffold(
+      bgColor: CommonColor.white,
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(top: 73),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 28),
+                  child: SvgPicture.asset(
+                    "assets/images/checkingmail.svg",
+                    height: 84,
+                    width: 84,
                   ),
-                   Text(CommonText.checkYourEmail,
-                      style: TextStyles.eighteenTSBlack),
-                  const SizedBox(
-                    height: 12,
-                  ),
-                   Text(CommonText.clickOnTheLink,
-                      style: TextStyles.sixteenTGrey),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 42),
+                ),
+                Text(CommonText.checkYourEmail,
+                    style: TextStyles.eighteenTSBlack),
+                const SizedBox(
+                  height: 12,
+                ),
+                Text(CommonText.clickOnTheLink, style: TextStyles.sixteenTGrey),
+                Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 42),
+                  child: InkWell(
+                    onTap: openGmailHomePage,
                     child: Container(
                       decoration: const BoxDecoration(
-                          color: Color(0xff1A56DB),
-                          borderRadius: BorderRadius.all(
-                            Radius.circular(8),
-                          ),),
+                        color: Color(0xff1A56DB),
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(8),
+                        ),
+                      ),
                       height: 48,
                       width: MediaQuery.of(context).size.width,
                       child: Center(
@@ -57,12 +61,24 @@ class _CheckEmailScreenState extends State<CheckEmailScreen> {
                       ),
                     ),
                   ),
-                ],
-              ),
+                ),
+              ],
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
+  }
+
+  void openGmailHomePage() async {
+    const String gmailUrl = 'https://mail.google.com/';
+    final Uri gmailAppUri = Uri.parse('googlegmail://inbox');
+    if (await canLaunchUrl(gmailAppUri)) {
+      await launchUrl(gmailAppUri);
+    } else if (await canLaunchUrl(Uri.parse(gmailUrl))) {
+      await launchUrl(Uri.parse(gmailUrl));
+    } else {
+      throw 'Could not launch Gmail';
+    }
   }
 }
--- lib/login_screen/Login_Screen.dart
+++ lib/login_screen/Login_Screen.dart
@@ -1,12 +1,9 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:kbox/Common/common_color.dart';
 import 'package:kbox/Common/common_scaffold.dart';
 import 'package:kbox/Common/common_text.dart';
 import 'package:kbox/Common/common_textfiled.dart';
-import 'package:kbox/employee_screen/choosepassword_screen/choosepasword_screen.dart';
 import 'package:kbox/employee_screen/choosepassword_screen/forget_screen.dart';
-import 'package:kbox/employee_screen/home_screen/homescreen.dart';
 import 'package:kbox/employee_screen/home_screen/main_activity.dart';
 
 import '../Common/text_style.dart';
@@ -27,154 +24,160 @@ class _LoginScreenState extends State<LoginScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: CommonScaffold(
-        bgColor: CommonColor.white,
-        body: Padding(
-          padding: const EdgeInsets.only(top: 90),
-          child: Form(
-            key: _formKey,
-            child: Column(
-              children: [
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      CommonText.login,
-                      style: TextStyles.eighteenTSBlack,
-                    ),
-                    const SizedBox(
-                      height: 30,
-                    ),
-                    Text(
-                      CommonText.email,
-                      style: TextStyles.fourteenTSBlackMedium,
-                    ),
-                    const SizedBox(
-                      height: 14,
-                    ),
-                    CommonTextFormField(
-                      controller: _emailController,
-                      labelText: CommonText.email,
-                      labelStyles: TextStyles.fourteenTSGrey,
-                      hintStyle: TextStyles.fourteenTSGrey,
-                      validator: (value) {
-                        if(value!.isEmpty){
-                          return CommonText.plzEnterEmail;
-                        }else if(!RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
-                            .hasMatch(_emailController.text) || value.contains(' ')) {
-                          return CommonText.plzEnterValidEmail;
-                        }else{
-                          return null;
-                        }
-                      },
-                    ),
-                    const SizedBox(
-                      height: 18,
-                    ),
-                    Text(
-                      CommonText.password,
-                      style: TextStyles.fourteenTSBlack,
-                    ),
-                    const SizedBox(
-                      height: 10,
-                    ),
-                    CommonTextFormField(
-                      controller: _passwordController,
-                      labelText: CommonText.enterYourPassword,
-                      labelStyles: TextStyles.fourteenTSGrey,
-                      hintStyle: TextStyles.fourteenTSGrey,
-                      keyboardType: TextInputType.visiblePassword,
-                      validator: (value) {
-                        if(value!.isEmpty || value.contains(' ')){
-                          return CommonText.plzEnterPass;
-                        }else{
-                          return null;
-                        }
-                      },
-
-                      suffixIcon: InkWell(
-                        onTap: () {
-                          setState(() {
-                            if(passVisibility == false){
-                              passVisibility = true;
-                            }else{
-                              passVisibility = false;
-                            }
-                        });
-                        },
-                        child: Icon(
-                          passVisibility == false? Icons.visibility_outlined:Icons.visibility_off_outlined,
-                          color: CommonColor.black,
-                        ),
-                      ),
-                      obscureText: passVisibility == false? true:false,
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.only(top: 31, bottom: 19),
-                      child: InkWell(
-                        onTap: () {
-                          if(_formKey.currentState!.validate()){
-                            Navigator.push(context, MaterialPageRoute(builder: (context) =>  MainActivity(),));
+    return CommonScaffold(
+      bgColor: CommonColor.white,
+      body: Padding(
+        padding: const EdgeInsets.only(top: 90),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    CommonText.login,
+                    style: TextStyles.eighteenTSBlack,
+                  ),
+                  const SizedBox(
+                    height: 30,
+                  ),
+                  Text(
+                    CommonText.email,
+                    style: TextStyles.fourteenTSBlackMedium,
+                  ),
+                  const SizedBox(
+                    height: 14,
+                  ),
+                  CommonTextFormField(
+                    controller: _emailController,
+                    labelText: CommonText.email,
+                    labelStyles: TextStyles.fourteenTSGrey,
+                    hintStyle: TextStyles.fourteenTSGrey,
+                    validator: (value) {
+                      if (value!.isEmpty) {
+                        return CommonText.plzEnterEmail;
+                      } else if (!RegExp(
+                                  r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+                              .hasMatch(_emailController.text) ||
+                          value.contains(' ')) {
+                        return CommonText.plzEnterValidEmail;
+                      } else {
+                        return null;
+                      }
+                    },
+                  ),
+                  const SizedBox(
+                    height: 18,
+                  ),
+                  Text(
+                    CommonText.password,
+                    style: TextStyles.fourteenTSBlack,
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  CommonTextFormField(
+                    controller: _passwordController,
+                    labelText: CommonText.enterYourPassword,
+                    labelStyles: TextStyles.fourteenTSGrey,
+                    hintStyle: TextStyles.fourteenTSGrey,
+                    keyboardType: TextInputType.visiblePassword,
+                    validator: (value) {
+                      if (value!.isEmpty || value.contains(' ')) {
+                        return CommonText.plzEnterPass;
+                      } else {
+                        return null;
+                      }
+                    },
+                    suffixIcon: InkWell(
+                      onTap: () {
+                        setState(() {
+                          if (passVisibility == false) {
+                            passVisibility = true;
+                          } else {
+                            passVisibility = false;
                           }
-                        },
-                        child: Container(
-                          decoration: BoxDecoration(
-                              color: CommonColor.blueWhiteColor,
-                              borderRadius: const BorderRadius.all(
-                                Radius.circular(8),
-                              )),
-                          height: 41,
-                          width: MediaQuery.of(context).size.width,
-                          child: Center(
-                            child: Text(
-                              CommonText.login,
-                              style: TextStyles.fourteenTSWhite,
-                            ),
+                        });
+                      },
+                      child: Icon(
+                        passVisibility == false
+                            ? Icons.visibility_outlined
+                            : Icons.visibility_off_outlined,
+                        color: CommonColor.black,
+                      ),
+                    ),
+                    obscureText: passVisibility == false ? true : false,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(top: 31, bottom: 19),
+                    child: InkWell(
+                      onTap: () {
+                        if (_formKey.currentState!.validate()) {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => MainActivity(),
+                              ));
+                        }
+                      },
+                      child: Container(
+                        decoration: BoxDecoration(
+                            color: CommonColor.blueWhiteColor,
+                            borderRadius: const BorderRadius.all(
+                              Radius.circular(8),
+                            )),
+                        height: 41,
+                        width: MediaQuery.of(context).size.width,
+                        child: Center(
+                          child: Text(
+                            CommonText.login,
+                            style: TextStyles.fourteenTSWhite,
                           ),
                         ),
                       ),
                     ),
-                    Align(
-                      alignment: Alignment.center,
-                      child: InkWell(
-                        onTap: () {
-                          Navigator.of(context).push(
-                            MaterialPageRoute(
-                              builder: (context) => const ForgetPasswordScreen(),
-                            ),
-                          );
-                        },
-                        child: Text(
-                          CommonText.forgetYourPassword,
-                          style: TextStyles.fourteenTSBlue,
-                        ),
+                  ),
+                  Align(
+                    alignment: Alignment.center,
+                    child: InkWell(
+                      onTap: () {
+                        Navigator.of(context).push(
+                          MaterialPageRoute(
+                            builder: (context) => const ForgetPasswordScreen(),
+                          ),
+                        );
+                      },
+                      child: Text(
+                        CommonText.forgetYourPassword,
+                        style: TextStyles.fourteenTSBlue,
                       ),
+                    ),
+                  ),
+                ],
+              ),
+              const Spacer(),
+              Padding(
+                padding: const EdgeInsets.only(bottom: 22),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Text(
+                      CommonText.integritypolicy,
+                      style: TextStyles.fourteenTSBlue,
+                    ),
+                    SizedBox(
+                      height: 13,
+                    ),
+                    Text(
+                      "${CommonText.version} 1.6.1",
+                      style: TextStyles.fourteenTSGrey,
                     ),
                   ],
                 ),
-                const Spacer(),
-                Padding(
-                  padding: EdgeInsets.only(bottom: 22),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    mainAxisAlignment: MainAxisAlignment.end,
-                    children: [
-                      Text(
-                        CommonText.integritypolicy,
-                        style: TextStyles.fourteenTSBlue,
-                      ),
-                      SizedBox(height: 13,),
-                      Text(
-                        "${CommonText.version} 1.6.1",
-                        style: TextStyles.fourteenTSGrey,
-                      ),
-
-                    ],
-                  ),
-                ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
       ),
